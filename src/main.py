928f30eda1d7834e3de42f2cad97486dc2b5dfd3
Eunho Lee <hiveruby@gmail.com>
2024-10-16 20:26:22 +0900

    Add pages

--- src/main.py
+++ src/main.py
@@ -1,52 +1,65 @@
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.middleware.gzip import GZipMiddleware
 
 app = FastAPI()
+app.add_middleware(GZipMiddleware)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 chapters = [
-    ["01장 파이썬이란 무엇인가?", "01-1 파이썬이란?", "01-2 파이썬의 특징", "01-3 파이썬으로 무엇을 할 수 있을까?", "01-4 파이썬 설치하기", "01-5 파이썬 둘러보기",
-     "01-6 파이썬과 에디터"],
-    ["02장 파이썬 프로그래밍의 기초, 자료형", "02-1 숫자형", "02-2 문자열 자료형", "02-3 리스트 자료형", "02-4 튜플 자료형"],
-    ["1", "2", "3", "90asd0fa09sd7f0as7df0ad7fs0"]
+    [
+        "들어가기에 앞서",
+        "0-1 작가의 말",
+        "0-2 자연어의 모호성",
+        "0-3 명령집합과 기계어"
+    ],
+    [
+        "C언어 시작하기",
+        "1-1 Hello, C!",
+        "1-2 변수와 산술",
+        "1-3 입출력",
+        "1-4 조건문",
+        "1-5 반복문",
+        "1-6 블럭",
+        "1-7 함수"
+    ],
+    [
+        "한 걸음 더",
+        "2-1 비트 연산자",
+        "2-2 포인터",
+        "2-3 형변환",
+        "2-4 배열",
+        "2-5 동적 할당",
+        "2-6 복합 연산자",
+        "2-7 증감 연산자",
+        "2-8 문자열과 문자열 리터럴",
+        "2-9 전처리문",
+        "2-10 소스와 헤더",
+    ],
+    [
+        "연습해보기",
+        "3-1 별 찍기",
+        "3-2 소수 만들기",
+        "3-3 문자열 치환",
+    ]
 ]
-
-
-# @app.exception_handler(Exception)
-# async def general_exception_handler(request: Request, exc: Exception):
-#     return RedirectResponse(url="/")
-#
-#
-# @app.exception_handler(StarletteHTTPException)
-# async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
-#     RedirectResponse(url="/")
-#
-#
-# @app.middleware("http")
-# async def add_exception_handling_middleware(request: Request, call_next):
-#     if not call_next:
-#         return RedirectResponse(url="/")
-#     try:
-#         response = await call_next(request)
-#     except:
-#         return RedirectResponse(url="/")
-#     if response.status_code == 404:
-#         return RedirectResponse(url="/")
-#     return response
 
 
 @app.get("/", response_class=HTMLResponse)
 async def render_main(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "chapters": chapters})
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "chapters": chapters}
+    )
 
 
 @app.get("/page/{id}")
 async def render_page(request: Request, id: int):
-    with open(f"pages/{id}.md", "r") as fp:
+    with open(f"pages/{id}.md", "r", encoding='utf-8') as fp:
         page_data = fp.read()
-    return templates.TemplateResponse(f"page.html", {"request": request, "chapters": chapters, "content": page_data})
+    return templates.TemplateResponse(
+        f"page.html", {"request": request, "chapters": chapters, "content": page_data}
+    )
